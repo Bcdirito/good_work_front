2ac0fb12f390d5fe3efd44933a759281fc7e9567
Brian DiRito <bdirito@gmail.com>
2018-12-13 14:03:09 -0500

    Navbar skeleton

--- src/components/general/Navbar.js
+++ src/components/general/Navbar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import {NavLink} from "react-router-dom"
 export default class NavBar extends Component {
   render() {
     return (
       <div>
-        This is where the NavBar will go
+        <NavLink to="/" className="nav-item">Home</NavLink>
+        <NavLink to="/doctors" className="nav-item">Doctors</NavLink>
+        <NavLink to="/groups" className="nav-item">Groups</NavLink>
       </div>
     )
   }

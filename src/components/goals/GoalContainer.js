3bb6590e7cabc0146db07a9ce8ff7f876ef8e016
Brian DiRito <bdirito@gmail.com>
2018-12-13 11:08:26 -0500

    implemented add goal redux logic

--- src/components/goals/GoalContainer.js
+++ src/components/goals/GoalContainer.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react'
+import Goal from "./Goal"
 import {connect} from "react-redux"
 
 class GoalContainer extends Component {
+    state = {
+        clicked: false,
+        formData: {
+            title: ""
+        }
+    }
+
+    buttonHandler = () => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+
+    changeHandler = e => {
+        this.setState({ formData: {
+            [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        this.props.addGoal(this.state.formData)
+    }
+
+    renderForm = () => {
+        return <form onSubmit={e => this.submitHandler(e)}>
+                <label>Title</label>
+                    <input type="text" name="title" value={this.state.formData.title} placeholder="Add Title Here" onChange={e => this.changeHandler(e)}/>
+                <input type="submit"></input>
+                </form>
+    }
+
+    deleteGoal = (goal) => {
+        this.props.deleteGoal(goal)
+    }
+
   render() {
     console.log(this.props)
+    // let allGoals = this.props.user.goals.map(goal => {
+    //     return <Goal
+    //             key={goal.id}
+    //             goal={goal}
+    //             deleteGoal={this.props.deleteGoal}
+    //             />
+    // })
     return (
       <div>
         This is the GoalContainer
+        {this.state.clicked === false ? <button onClick={this.buttonHandler}>Add Goal</button>: null}
+        <div>{this.state.clicked === true ? this.renderForm() : null}</div>
       </div>
     )
   }
@@ -18,4 +63,11 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(GoalContainer)
+const mapDispatchToProps = dispatch => {
+    return {
+        addGoal: goal => dispatch({type: "ADD_GOAL", goal}),
+        deleteGoal: goal => dispatch({type: "DELETE_GOAL", goal})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalContainer)
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -5,6 +5,12 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case "ADD_GOAL":
+            console.log("Triggered Add Goal", action.goal)
+    
+        case "DELETE_GOAL":
+            console.log("Triggered Delete Goal")
+
         default:
             return state
     }

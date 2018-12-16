fb8110530cd1a9b4cf4902997f0823521db9827e
Brian DiRito <bdirito@gmail.com>
2018-12-16 10:41:53 -0500

    home container functionality

--- src/general/Home.js
+++ src/general/Home.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
 import GoalContainer from "../goals/GoalContainer"
+import { connect } from "react-redux"
+import { Button } from 'semantic-ui-react';
 
 class Home extends Component {
+  clickHandler = e => {
+    this.props.history.replace(`/${e.target.name}`)
+  }
+
+  homeRender = () => {
+    if (this.props.user.length === undefined) {
+      return (<div>
+              <Button name="login" onClick={e => this.clickHandler(e)}>Login/Sign Up</Button>
+              <Button name="doctors" onClick={e => this.clickHandler(e)}>Find A Doctor</Button>
+              </div>)
+    } else {
+      return <GoalContainer />
+    }
+  }
+
   render() {
     return (
       <div>
-        <GoalContainer />
+        {this.homeRender()}
       </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(Home)
